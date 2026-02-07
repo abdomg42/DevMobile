86fa8554240f52cca9e8b82453ebe294bba50b16
mg <moghandez123@gmail.com>
2026-02-07 16:23:34 +0100

    start

---
+++ app/src/main/java/com/example/firstapp1/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.firstapp1;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    Button b2;
+    TextView t2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        b2=(Button)findViewById(R.id.b1);
+        t2=(TextView) findViewById(R.id.t1);
+        b2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                t2.setText("Bonsoir");
+            } });
+
+        EdgeToEdge.enable(this);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
